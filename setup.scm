e491be8d73be5b353557f0d50bbfdc7c4e3be72d
Bryan Ferris <saffsnail@gmail.com>
2017-09-28 10:32:05 -0700

    Updated setup to reflect changes in home-project

--- setup.scm
+++ setup.scm
@@ -1,3 +1,3 @@
-(safe-symlink (in-dir dotdir '("zsh/init.zsh"))    (in-dir home '(".zshrc")))
-(safe-symlink (in-dir dotdir '("zsh/profile.zsh")) (in-dir home '(".zprofile")))
+(safe-symlink (in-dir dotdir "zsh/init.zsh")    (in-dir home ".zshrc"))
+(safe-symlink (in-dir dotdir "zsh/profile.zsh") (in-dir home ".zprofile"))
 
